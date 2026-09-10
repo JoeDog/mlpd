3e38cba7291b26926c39cb3400839ee57dbab896
Jeff Fulmer <jeff@joedog.org>
2026-09-10 10:22:46 -0400

    Added to the distro

---
+++ src/org/joedog/ann/Version.java
@@ -0,0 +1,27 @@
+package org.joedog.ann;
+/**
+ * Copyright (C) 2026
+ * Jeffrey Fulmer - <jeff@joedog.org>, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *--
+ */
+
+public final class Version {
+  public final static String version   = "1.0.1";
+  public final static String copyright = "2026";
+  public final static String author    = "Jeffrey Fulmer, et al.";
+  public final static String email     = "jeff@joedog.org";
+}
